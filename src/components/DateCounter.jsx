ef08880f40056f3ebbc37f1375fe20f72d05a031
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2025-01-02 15:29:58 +0200

    feat: make a central palce for actions - use switch instead of multiple if - conver use state to use reducer

--- src/components/DateCounter.jsx
+++ src/components/DateCounter.jsx
@@ -1,15 +1,32 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
+
+const initialState = { count: 0, step: 1 };
 
 function reducer(state, action) {
-  if (action.type === "increment") return state + 1;
-  if (action.type === "decrement") return state - 1;
-  if (action.type === "setCount") return action.payload;
-  if (action.type === "reset") return action.payload;
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + state.step };
+    case "decrement":
+      return { ...state, count: state.count - state.step };
+    case "setCount":
+      return { ...state, count: action.payload };
+    case "setStep":
+      return { ...state, step: action.payload };
+    case "reset":
+      return initialState;
+    default:
+      throw new Error("unknown action");
+  }
+  // if (action.type === "increment") return state + 1;
+  // if (action.type === "decrement") return state - 1;
+  // if (action.type === "setCount") return action.payload;
+  // if (action.type === "reset") return action.payload;
 }
 function DateCounter() {
   // const [count, setCount] = useState(0);
-  const [count, dispatch] = useReducer(reducer, 0);
-  const [step, setStep] = useState(1);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
+  // const [step, setStep] = useState(1);
 
   // This mutates the date object.
   const date = new Date("jan 1 2025");
@@ -33,12 +50,11 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
   };
 
   const reset = function () {
-    dispatch({ type: "reset", payload: 0 });
-    setStep(1);
+    dispatch({ type: "reset" });
   };
 
   return (
